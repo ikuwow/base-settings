81c2996997a1f382219298cffc19875ae804d06c
Ikuo Degawa <degawa@ikuwow.com>
2015-03-22 01:01:19 +0900

    include selinux cookbook and disabled

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 name             'base-settings'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+depends          'selinux', '~> 0.9.0'
+maintainer       'ikuwow'
+maintainer_email 'ikuwow'
 license          'All rights reserved'
 description      'Installs/Configures base-settings'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,5 @@
 #
 
 include_recipe "base-settings::user"
+
+include_recipe "selinux::disabled"
