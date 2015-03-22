59bc6f536a0ab77a6909ede4db4b6be8efc42f31
Ikuo Degawa <degawa@ikuwow.com>
2015-03-22 11:51:41 +0900

    add iku and wow user

--- recipes/user.rb
+++ recipes/user.rb
@@ -16,3 +16,14 @@ user "ikuwow" do
     home "/home/ikuwow"
     action :create
 end
+
+user_list = data_bag('users')
+
+user_list.each do |ul|
+    u = data_bag_item('users',ul)
+    user u["id"] do
+        comment u["comment"]
+        home "/home/#{u["id"]}"
+        action :create
+    end
+end
--- test/integration/default/serverspec/user_spec.rb
+++ test/integration/default/serverspec/user_spec.rb
@@ -6,3 +6,9 @@ describe user('ikuwow') do
     it { should belong_to_group 'ikuwow' }
     it { should have_home_directory '/home/ikuwow' }
 end
+
+%w{iku wow}.each do |u|
+    describe user(u) do
+        it { should exist}
+    end
+end
